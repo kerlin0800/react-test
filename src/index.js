242e697a0c77f13a99753627ac47dffb23828174
Karol <kerlin0800@gmail.com>
2019-06-27 15:05:44 +0200

    dodanie bootstrap

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { Button } from "reactstrap";
 
+import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
 
 export class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <header>
+          <h1>Hello CodeSandbox</h1>
+        </header>
+        <main>
+          <Button color="success">Tak</Button>
+          <Button color="danger">Nie</Button>
+        </main>
       </div>
     );
   }
